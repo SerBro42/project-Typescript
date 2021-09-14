372a223aa867adee5d19cb30979324e692f31a3c
Sergey Brovko <serbro7@gmail.com>
2021-09-14 13:47:35 +0200

    Sum function
    Added sum function snippet

--- app.ts
+++ app.ts
@@ -1,1 +1,12 @@
 console.log('Time to get started!')
+
+//First of all, a number type test:
+function add(n1, n2) {
+    return n1 + n2;
+}
+
+let number1 = 5;
+let number2 = 2.8;
+
+let result = add(number1, number2);
+console.log(result);
