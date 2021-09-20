0564278d914f5f1a4d435677d7574d2377218a65
Sergey Brovko <serbro7@gmail.com>
2021-09-20 14:31:11 +0200

    Arrays
    Added a string array to the "person" object. It made up of strings exclusively, and it behaves as such (not allowing elements of eny other type, etc.)

--- app.js
+++ app.js
@@ -6,6 +6,18 @@
 //This syntax is the better syntax:
 var person = {
     name: 'Sergey',
-    age: 31
+    age: 31,
+    hobbies: ['Wargaming', 'Programming']
 };
+//This variable is declared as an array of strings
+var favouriteActivities;
+favouriteActivities = ['Sports'];
 console.log(person.name);
+//This for loop is familiar to any JavaScript user:
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    //toUpperCase() method is allowed because we established the array as a sting array
+    console.log(hobby.toUpperCase());
+    //map() method is not allowed because it's a string array
+    //console.log(hobby.map()) // !!! ERROR
+}
--- app.ts
+++ app.ts
@@ -7,7 +7,20 @@
 //This syntax is the better syntax:
 const person = {
     name: 'Sergey',
-    age: 31
+    age: 31,
+    hobbies: ['Wargaming', 'Programming']
 };
 
+//This variable is declared as an array of strings
+let favouriteActivities: string[];
+favouriteActivities = ['Sports'];
+
 console.log(person.name);
+
+//This for loop is familiar to any JavaScript user:
+for (const hobby of person.hobbies) {
+    //toUpperCase() method is allowed because we established the array as a sting array
+    console.log(hobby.toUpperCase());
+    //map() method is not allowed because it's a string array
+    //console.log(hobby.map()) // !!! ERROR
+}
